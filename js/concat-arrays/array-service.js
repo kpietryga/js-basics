3bf111b4c3353447f28e4dab87dcd62234ab1dbc
kpietryga <krzysztof.pietryga@gmail.com>
2024-09-26 21:17:25 +0200

    Finds the element in the array

--- js/concat-arrays/array-service.js
+++ js/concat-arrays/array-service.js
@@ -1,3 +1,6 @@
+import {EratosthenesSieve} from "./eratosthenes-sieve";
+import {Point} from "./point";
+
 export class ArrayService {
     constructor(arr1, arr2) {
         this.mergedArr = this.merge(arr1, arr2);
@@ -37,6 +40,98 @@ export class ArrayService {
             return [...row, ...zerosArr]
         })
     }
+
+    /**
+     * Finds the element in the array whose neighbors' sum is the largest.
+     * It searches all neighbors of a given element and then selects the one with the highest sum.
+     *
+     * @returns {Object} An object containing the neighbors' sum (`sum`), row number (`row`), and column number (`col`) of the element with the largest neighbors' sum.
+     */
+    findMaxSumNeighbors() {
+        const maxSumNeighbor = {sum: 0, row: 0, col: 0}
+
+        for (let row = 0; row < this.mergedArr.length; row++) {
+            for (let col = 0; col < this.mergedArr[0].length; col++) {
+
+                const neighbors = [
+                    this.#getNeighbor(this.mergedArr, row - 1, col - 1),  // top left
+                    this.#getNeighbor(this.mergedArr, row - 1, col),      // top center
+                    this.#getNeighbor(this.mergedArr, row - 1, col + 1),  // top right
+
+                    this.#getNeighbor(this.mergedArr, row, col - 1),      // middle left
+                    // this.#getNeighbor(this.mergedArr, row, col),       // middle center
+                    this.#getNeighbor(this.mergedArr, row, col + 1),      // middle right
+
+                    this.#getNeighbor(this.mergedArr, row + 1, col - 1),  // bottom left
+                    this.#getNeighbor(this.mergedArr, row + 1, col),      // bottom center
+                    this.#getNeighbor(this.mergedArr, row + 1, col + 1)   // bottom right
+
+                ].filter(neighbor => neighbor !== null);  // usuń tych co nie istnieją
+
+                const sum = neighbors.reduce((acc, curr) => acc + curr)
+                if (sum > maxSumNeighbor.sum) {
+                    maxSumNeighbor.sum = sum;
+                    maxSumNeighbor.row = row;
+                    maxSumNeighbor.col = col;
+                }
+            }
+        }
+        return maxSumNeighbor;
+    }
+
+    /**
+     * A private method that returns a neighbor in the array.
+     * Returns null if the neighbor does not exist (when the coordinates are out of bounds).
+     *
+     * @param {number[][]} arr - The array from which the neighbor is fetched.
+     * @param {number} x - The x-coordinate (row) of the neighbor.
+     * @param {number} y - The y-coordinate (column) of the neighbor.
+     * @returns {number|null} The value of the neighbor or null if the neighbor does not exist.
+     */
+    #getNeighbor = (arr, x, y) => {
+        if (x < 0 || y < 0 || x > arr.length - 1 || y > arr[0].length - 1) {
+            return null;
+        }
+        return arr[x][y];
+    }
+
+    /**
+     * Finds the largest element in the array.
+     * If there are multiple such elements, it selects the one closest to the top-left corner of the array.
+     *
+     * @returns {Object} An object containing the maximum value (`maxValue`) and the closest point (`result`).
+     * @throws {Error} If the array is empty or invalid.
+     */
+    getMax() {
+        if (!this.mergedArr || this.mergedArr.length === 0) {
+            throw new Error('Tablica jest pusta albo nie prawidłowa')
+        }
+
+        let maxValue = 0
+        let maxValuePoints = []
+
+        for (let row = 0; row < this.mergedArr.length; row++) {
+            for (let col = 0; col < this.mergedArr[0].length; col++) {
+                const value = this.mergedArr[row][col];
+                if (value > maxValue) {
+                    maxValue = value;
+                    maxValuePoints = [new Point(row, col)]
+                } else if (value === maxValue) {
+                    maxValuePoints = [...maxValuePoints, new Point(row, col)];
+                }
+            }
+        }
+
+
+        const leftTopPoint = new Point(0, 0);
+        const result = this.#findLowestDistanceToPoint(maxValuePoints, leftTopPoint);
+        return {
+            maxValue,
+            result
+        }
+
+    }
+
 
 }
 
