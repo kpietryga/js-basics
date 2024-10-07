5d4b5ab85e735711f80d234499789238af04cfd4
kpietryga <krzysztof.pietryga@gmail.com>
2024-10-07 22:46:03 +0200

    Service for analyzing a team's matches

--- js/teams/team-marches-service.js
+++ js/teams/team-marches-service.js
@@ -39,83 +39,111 @@ export const teamMatchesService = (team) => {
         return maxStreak;
     };
 
-};
-
-/**
- * Calculates the goal balance of the team.
- *
- * @returns {Object} - An object containing the number of goals scored by the team and conceded by the team.
- */
-const getGoalBalance = () => {
-    return matches.reduce((acc, curr) => {
-        if (curr.isHome()) {
-            acc.teamGoals += curr.homeGoals;
-            acc.opponentGoals += curr.awayGoals;
-        } else {
-            acc.teamGoals += curr.awayGoals;
-            acc.opponentGoals += curr.homeGoals;
-        }
-        return acc;
-    }, {teamGoals: 0, opponentGoals: 0});
-};
-
-/**
- * Calculates the average attendance at the team's matches.
- *
- * @returns {number} - Average attendance at matches.
- */
-const getAverageAttendance = () => {
-    const sum = matches.reduce((sum, match) => sum + match.audience, 0);
-    return sum / matches.length;
-};
-
-/**
- * Calculates the win balance at home and away.
- *
- * @returns {Object} - An object containing the number of home and away wins.
- */
-const getHomeAwayWinBalance = () => {
-    return matches.reduce((acc, curr) => {
-        if (curr.isHomeWinner()) {
-            acc.home++;
-        } else if (curr.isAwayWinner()) {
-            acc.away++;
-        }
-        return acc;
-    }, {home: 0, away: 0});
-};
-
-/**
- * Calculates the biggest win and biggest loss for the team
- *
- * @returns {Object} - An object containing the biggest win and biggest loss.
- */
-const getBiggestWinAndLoss = () => {
-    return matches.reduce((acc, curr) => {
-        let diff = curr.homeGoals - curr.awayGoals;
-
-        if (diff === 0) {
+    /**
+     * Calculates the goal balance of the team.
+     *
+     * @returns {Object} - An object containing the number of goals scored by the team and conceded by the team.
+     */
+    const getGoalBalance = () => {
+        return matches.reduce((acc, curr) => {
+            if (curr.isHome()) {
+                acc.teamGoals += curr.homeGoals;
+                acc.opponentGoals += curr.awayGoals;
+            } else {
+                acc.teamGoals += curr.awayGoals;
+                acc.opponentGoals += curr.homeGoals;
+            }
             return acc;
-        }
+        }, {teamGoals: 0, opponentGoals: 0});
+    };
 
-        if (curr.isAway()) {
-            diff *= (-1);
-        }
+    /**
+     * Calculates the average attendance at the team's matches.
+     *
+     * @returns {number} - Average attendance at matches.
+     */
+    const getAverageAttendance = () => {
+        const sum = matches.reduce((sum, match) => sum + match.audience, 0);
+        return sum / matches.length;
+    };
 
-        if (diff > acc.win.diff) {
-            acc.win.diff = diff;
-            acc.win.result = [curr];
-        } else if (diff === acc.win.diff) {
-            acc.win.result.push(curr);
-        }
+    /**
+     * Calculates the win balance at home and away.
+     *
+     * @returns {Object} - An object containing the number of home and away wins.
+     */
+    const getHomeAwayWinBalance = () => {
+        return matches.reduce((acc, curr) => {
+            if (curr.isHomeWinner()) {
+                acc.home++;
+            } else if (curr.isAwayWinner()) {
+                acc.away++;
+            }
+            return acc;
+        }, {home: 0, away: 0});
+    };
 
-        if (diff < acc.loss.diff) {
-            acc.loss.diff = diff;
-            acc.loss.result = [curr];
-        } else if (diff === acc.loss.diff) {
-            acc.loss.result.push(curr);
-        }
+    /**
+     * Calculates the biggest win and biggest loss for the team.
+     *
+     * @returns {Object} - An object containing the biggest win and biggest loss.
+     */
+    const getBiggestWinAndLoss = () => {
+        return matches.reduce((acc, curr) => {
+            let diff = curr.homeGoals - curr.awayGoals;
 
-        return acc;
-    }, {win: {diff: 0, result: []}, loss: {diff: 0, result: []}});
+            if (diff === 0) {
+                return acc;
+            }
+
+            if (curr.isAway()) {
+                diff *= (-1);
+            }
+
+            if (diff > acc.win.diff) {
+                acc.win.diff = diff;
+                acc.win.result = [curr];
+            } else if (diff === acc.win.diff) {
+                acc.win.result.push(curr);
+            }
+
+            if (diff < acc.loss.diff) {
+                acc.loss.diff = diff;
+                acc.loss.result = [curr];
+            } else if (diff === acc.loss.diff) {
+                acc.loss.result.push(curr);
+            }
+
+            return acc;
+        }, {win: {diff: 0, result: []}, loss: {diff: 0, result: []}});
+    };
+
+    /**
+     * Calculates statistics related to goals.
+     *
+     * @returns {Object} - An object containing the average number of minutes between goals scored and conceded.
+     */
+    const calculateGoalStatistics = () => {
+        const {teamGoals, opponentGoals} = getGoalBalance();
+
+        const totalMinutes = matches.length * 90;
+
+        const minutesPerGoalScored = totalMinutes / teamGoals;
+        const minutesPerGoalLosses = totalMinutes / opponentGoals;
+
+        return {
+            minutesPerGoalScored,
+            minutesPerGoalLosses
+        };
+    };
+
+    return {
+        getPoints,
+        getLongestHomeUnbeatenStreak,
+        getGoalBalance,
+        getAverageAttendance,
+        getHomeAwayWinBalance,
+        getBiggestWinAndLoss,
+        calculateGoalStatistics
+    };
 };
