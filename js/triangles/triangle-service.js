c9886759122dde652392b0dd9bd593c1dcbf2d58
kpietryga <krzysztof.pietryga@gmail.com>
2024-10-14 21:29:36 +0200

    Finds triangles with the minimum and maximum area

---
+++ js/triangles/triangle-service.js
@@ -0,0 +1,20 @@
+/**
+     * Finds triangles with the minimum and maximum area and calculates the ratio of the smallest to the largest area.
+     * @returns {Object} - Object containing the minimum area, maximum area, and the ratio.
+     */
+    const findMinMaxArea = () => {
+        const minMax = sidesArr.reduce((acc, curr) => {
+            // p - half of the triangle's perimeter
+            const area = curr.calcArea();
+            if (acc.min > area) acc.min = area;
+            if (acc.max < area) acc.max = area;
+            return acc;
+        }, { min: Infinity, max: 0 });
+
+        return {
+            minArea: minMax.min,
+            maxArea: minMax.max,
+            ratio: minMax.min / minMax.max,
+        };
+    };
+
