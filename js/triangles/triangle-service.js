feead0a8032566fc7a76c3adaf54b13beb389d34
kpietryga <krzysztof.pietryga@gmail.com>
2024-10-15 21:52:12 +0200

    Finds triangles with the largest perimeter

--- js/triangles/triangle-service.js
+++ js/triangles/triangle-service.js
@@ -18,3 +18,32 @@
         };
     };
 
+/**
+ * Filters right-angled triangles from the list.
+ * @returns {TriangleSides[]} - Array of right-angled triangles.
+ */
+const findRightAngledTriangles = () => sidesArr.filter(t => t.isRightAngled());
+
+/**
+ * Finds triangles with the largest perimeter.
+ * @param {TriangleSides[]} arr - Array of triangles.
+ * @returns {Object} - Object containing the maximum perimeter and an array of triangles with that perimeter.
+ */
+const maxPerimeter = (arr) => arr.reduce((acc, curr) => {
+    const currPer = curr.calcPerimeter();
+    if (curr.hasPerimeterGreaterThan(acc.maxValue)) {
+        acc.maxValue = currPer;
+        acc.maxTriangles = [curr];
+    } else if (currPer === acc.maxValue) {
+        acc.maxTriangles.push(curr);
+    }
+    return acc;
+}, {
+    maxValue: 0,
+    maxTriangles: []
+});
+
+
+
+
+
